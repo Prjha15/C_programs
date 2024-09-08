17aec9085c430493f016fb46afffbdeb09c9cd0b
Prjha15 <priyankajha1516@gmail.com>
2024-09-08 15:44:57 +0530

    Add files via upload

---
+++ printing_n_numbers_using_recursions.c
@@ -0,0 +1,15 @@
+#include<stdio.h> 
+int n=1;
+void num(){
+    printf("%d",n);
+
+    void num();
+    return n;
+}
+int main(){
+    printf("enter the value of n:");
+    scanf("%d",&n);
+    void num();
+
+    return 0;
+}
