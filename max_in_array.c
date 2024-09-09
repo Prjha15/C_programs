7ef5becb6910009e34ec5c826c0cac80f6738f66
Prjha15 <priyankajha1516@gmail.com>
2024-09-09 17:29:36 +0530

    Update max_in_array.c

--- max_in_array.c
+++ max_in_array.c
@@ -2,13 +2,13 @@
 int main(){
     int arr[5]={23,56,76,45,77};
     int max;
+    max=arr[0];
     int i;
     for (i=0;i<=4;i++){
-        max=arr[0];
         if(arr[i]>max){
             max=arr[i];
         }
     }
-    printf("MAXIMUM AMONG ELEMENTS IS %d",arr[i]);
+    printf("MAXIMUM AMONG ELEMENTS IS %d",max);
     return 0;
 }
